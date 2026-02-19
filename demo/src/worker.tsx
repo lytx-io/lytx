001df7f238799dddb884a8c1742da269e753c40a
JonathanRiche <j.riche64@gmail.com>
2026-02-19 09:36:28 -0500

    chore(repo): track local opencode config and demo note

--- demo/src/worker.tsx
+++ demo/src/worker.tsx
@@ -2,6 +2,7 @@ import { createLytxApp, SyncDurableObject, SiteDurableObject } from "@lytx/core"
 
 export { SyncDurableObject, SiteDurableObject };
 
+//TODO: show other types of combinatiosn ie clickhouse/singlestore etc with redis over kv etc
 const app = createLytxApp({
   db: {
     dbAdapter: "sqlite",
